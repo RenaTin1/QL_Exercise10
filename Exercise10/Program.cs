9cd6ba61b6c70557911adcbeccc8a55cf42607bc
RenaTin1 <31945448+RenaTin1@users.noreply.github.com>
2017-10-04 16:20:49 -0400

    Exercise 10 (First Commit)

---
+++ Exercise10/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Serena Tindle- Exercise 10 -- Have user input if they're over 54' tall. If not they cannot ride the Raptor.
+
+           bool ContinueProgram = true; //Declare and intialize bool
+
+           while (ContinueProgram == true) //While statement
+           { 
+                //Variables
+            double height;
+            string UserOption;
+
+            Console.WriteLine("Welcome to Raptor!!");
+            Console.WriteLine("Please enter your height in inches: ");
+            height = double.Parse(Console.ReadLine()); //read user input
+                if (height < 54.0)
+                {
+                    Console.WriteLine("Sorry you cannot ride the Raptor! Try again Next year!");
+                    TryAgain(UserOption);//call method for asking if they want to try again
+                }
+                else
+                {
+                    Console.WriteLine("Great!! You can ride the Raptor!");
+                    TryAgain(UserOption);
+
+                }
+             
+           }
+ }
+
+        static void TryAgain(string UserOption)
+        {
+            Console.WriteLine("Would you like to Continue? (n/y)"); //Try again method
+            UserOption = Console.ReadLine();
+
+            if (UserOption == "N" || UserOption == "No" || UserOption == "no")
+            {
+                ContinueProgram = false;
+                Console.WriteLine("Goodbye!");
+            }
+
+        }
+
+
+    }
+    }
